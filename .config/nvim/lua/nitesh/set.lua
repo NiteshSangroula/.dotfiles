75812c3a07e88db109b7a9a4e7d22d6d07ce255b
Nitesh Sangroula <sangroulanitesh123@gmail.com>
2022-09-10 17:28:16 +0545

    change terminal colorscheme and some other minor change

--- .config/nvim/lua/nitesh/set.lua
+++ .config/nvim/lua/nitesh/set.lua
@@ -13,9 +13,33 @@ vim.opt.expandtab = true
 
 vim.opt.smartindent = true
 
-vim.opt.wrap = false
+vim.opt.wrap = true
 
 vim.g.mapleader = " "
 
-vim.opt.cmdheight = 2   --more space for displaying messages
+vim.opt.cmdheight = 2 --more space for displaying messages
+
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+vim.opt.undofile = true
+
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+
+vim.opt.termguicolors = true
+
+vim.opt.scrolloff = 8
+vim.opt.signcolumn = "yes"
+vim.opt.isfname:append("@-@")
+
+-- Having longer updatetime (default is 4000 ms = 4 s) leads to noticeable
+-- delays and poor user experience.
+vim.opt.updatetime = 50
+
+-- Don't pass messages to |ins-completion-menu|.
+vim.opt.shortmess:append("c")
+
+vim.opt.colorcolumn = "100"
+
 
